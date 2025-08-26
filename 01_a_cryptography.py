dce9aee38d483d126c46a3e8dd1b8b6644651b4f
Akash Kumar <121351972+akashkr16@users.noreply.github.com>
2025-08-26 19:12:59 +0530

    Update 01_a_cryptography.py

--- 01_a_cryptography.py
+++ 01_a_cryptography.py
@@ -1,2 +1,15 @@
 # AKASH KUMAR (2025PGCSIS09)
-# iMPLEMENTATION OF 
+# implementation of euclidean algorithm (gcd)
+
+def gcd(a, b):
+    while b != 0:
+        a, b = b, a % b
+    return a
+
+# Input
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+# Output
+print(f"GCD of {a} and {b} is: {gcd(a, b)}")
+
